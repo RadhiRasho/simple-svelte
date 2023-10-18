d5bcd2ccfdc56a312976ce4479cbc452a0a1d375
Radhi Rasho <radhi.h@outlook.com>
2023-10-18 17:43:47 +0000

    updating packages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,42 +1,163 @@
-<script>
-	import { count } from '$lib';
+<script lang="ts">
+	import { onMount } from 'svelte';
 
-	$: arr = new Array($count).fill(0);
+	let colors: string[] = ['red', 'green', 'blue', 'yellow', 'violet', 'orange'];
+	let images: string[] = [];
+
+	let color: string = 'red';
+	let size: number = 50;
+
+	let canvas: HTMLCanvasElement;
+	let context: CanvasRenderingContext2D | null;
+	let previous: { x: any; y: any } | null;
+
+	onMount(() => {
+		context = canvas.getContext('2d');
+
+		function resize() {
+			canvas.width = canvas.clientWidth;
+			canvas.height = canvas.clientHeight;
+		}
+
+		window.addEventListener('resize', resize);
+		resize();
+
+		return () => {
+			window.removeEventListener('resize', resize);
+		};
+	});
+
+	function get_coords(e: { clientX: any; clientY: any }) {
+		const { clientX, clientY } = e;
+		const { left, top } = canvas.getBoundingClientRect();
+		const x = clientX - left;
+		const y = clientY - top;
+		return { x, y };
+	}
+
+	function capture_state() {
+		const state = canvas.toDataURL();
+		images = [state, ...images];
+	}
 </script>
 
-<a href="./test">Test Page</a>
+<div class="colors">
+	{#each colors as selectable}
+		<button
+			class="color"
+			style="background: {selectable}"
+			class:active={color === selectable}
+			on:click={() => (color = selectable)}
+		/>
+	{/each}
+	<div class="range">
+		small
+		<input type="range" min="1" max="100" bind:value={size} />
+		large
+	</div>
+</div>
 
-<h1>The count is {$count}</h1>
+<div class="main-section">
+	<canvas
+		bind:this={canvas}
+		on:pointerdown={(e) => {
+			const coords = get_coords(e);
+			if (context === null) throw new Error('context is null');
 
-<button on:click={count.increment}>+</button>
-<button on:click={count.decrement}>-</button>
-<button on:click={count.reset}>reset</button>
-<br />
-<br />
+			context.fillStyle = color;
+			context?.beginPath();
+			context?.arc(coords.x, coords.y, size / 2, 0, 2 * Math.PI);
+			context?.fill();
 
-{#each arr as i, index}
-	<span>{i + index + 1}&nbsp;</span>
-{/each}
+			previous = coords;
+		}}
+		on:pointerup={capture_state}
+		on:pointerleave={() => (previous = null)}
+		on:pointermove={(e) => {
+			const coords = get_coords(e);
 
-{#if $count > 10}
-	<p>That's a lot of clicks!</p>
-{/if}
+			if (e.buttons === 1) {
+				e.preventDefault();
+
+				if (context === null) throw new Error('context is null');
+				context.strokeStyle = color;
+				context.lineWidth = size;
+				context.lineCap = 'round';
+				context?.beginPath();
+				context?.moveTo(previous?.x, previous?.y);
+				context?.lineTo(coords.x, coords.y);
+				context?.stroke();
+			}
+
+			previous = coords;
+		}}
+	/>
+	<div class="images">
+		{#each images as src, key}
+			<img alt={key.toString()} {src} />
+		{/each}
+	</div>
+</div>
 
 <style>
-	h1 {
-		color: red;
-	}
-
-	span,
-	p {
-		color: violet;
-	}
-
-	button {
-		color: green;
-	}
-
 	:global(body) {
 		background: black;
+	}
+
+	canvas {
+		background: #eee;
+		border: 1px solid #ccc;
+		width: 75vw;
+		height: 90vh;
+	}
+
+	.color {
+		border-radius: 100%;
+		height: 50px;
+		margin: 0 5px;
+		width: 50px;
+
+		&:active {
+			border: 1px solid white;
+		}
+	}
+
+	.colors {
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+		border: 1px solid red;
+		margin-bottom: 10px;
+		gap: 15px;
+	}
+
+	.main-section {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+	}
+
+	.images {
+		border: white 1px solid;
+		display: flex;
+		flex-direction: column;
+		height: 90vh;
+		width: 25vw;
+		overflow-y: auto;
+		gap: 10px;
+
+		& > img {
+			width: 100%;
+			background-color: transparent;
+			border: 1px solid white;
+		}
+	}
+
+	.range {
+		color: white;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		gap: 10px;
 	}
 </style>
